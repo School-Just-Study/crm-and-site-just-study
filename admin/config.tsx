47210f52c56f2311937f255ca9c866cd255f7a99
Каменев Николай Вячеславович <Nikolay.Kamenev@russianpost.ru>
2022-01-09 17:28:57 +0300

    feat: added keystone

---
+++ admin/config.tsx
@@ -0,0 +1,7 @@
+function CustomLogo() {
+    return <h3>Just Study CMS</h3>
+}
+
+export const components = {
+    Logo: CustomLogo
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+
+const {withKeystone} = require("@keystone-6/core/next");
+module.exports = withKeystone({
+    reactStrictMode: true,
+});
