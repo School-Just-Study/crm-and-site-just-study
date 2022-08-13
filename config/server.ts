e39f45be7893f95d6abaa45f9564cbdeb2b4b0c1
Nikolay Kamenev <kamenik1@icloud.com>
2022-08-13 15:42:58 +0300

    feat: remove Sentry

--- config/server.ts
+++ config/server.ts
@@ -35,22 +35,6 @@ export const server: KeystoneConfig["server"] = {
     credentials: true,
   },
   extendExpressApp: (app, createContext) => {
-    Sentry.init({
-      dsn: SENTRY_DNS,
-      tracesSampleRate: 1.0,
-      release: configProject.version,
-      environment: process.env.NODE_ENV,
-      autoSessionTracking: true,
-      integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        new Tracing.Integrations.Express({ app }),
-      ],
-    });
-
-    app.use(Sentry.Handlers.requestHandler());
-    app.use(Sentry.Handlers.tracingHandler());
-    app.use(Sentry.Handlers.errorHandler());
-
     app.use(bodyParser.json());
 
     app.post("/api/yookassa", async (req, res) => {
