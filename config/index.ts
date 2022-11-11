9659486c59517c7db15e2688f00e956d7c4fb03c
Nikolay Kamenev <kamenik1@icloud.com>
2022-11-11 15:52:06 +0300

    feat: added notification manager (#110)

--- config/index.ts
+++ config/index.ts
@@ -1,35 +1,30 @@
 export const SERVER_PORT = parseInt(process.env.PORT!) || 8000;
 
 export const DATABASE_URL =
-  process.env.DATABASE_URL || "mysql://root:root@localhost:8889/sitejuststudy";
+    process.env.DATABASE_URL || 'mysql://root:root@localhost:8889/sitejuststudy';
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
-export const BACKEND_URL = process.env.BACKEND_URL || "localhost:8000";
+export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+export const BACKEND_URL = process.env.BACKEND_URL || 'localhost:8000';
 
 export const NALOG_INN = process.env.NALOG_INN || 0;
-export const NALOG_PASSWORD = process.env.NALOG_PASSWORD || "";
+export const NALOG_PASSWORD = process.env.NALOG_PASSWORD || '';
 
 export const SESSION_SECRET =
-  process.env.SESSION_SECRET || "e21bada5-a1a1-4114-81fd-487be3ad6fca";
+    process.env.SESSION_SECRET || 'e21bada5-a1a1-4114-81fd-487be3ad6fca';
 
-export const S3_STORAGE_KEY_ID = process.env.S3_STORAGE_KEY_ID || "";
-export const S3_STORAGE_KEY_SECRET = process.env.S3_STORAGE_KEY_SECRET || "";
-
-export const MAIL_HOST = process.env.MAIL_HOST || "";
-export const MAIL_PORT = process.env.MAIL_PORT || "1";
-export const MAIL_USER = process.env.MAIL_USER || "ethereal.email";
-export const MAIL_PASS = process.env.MAIL_PASS || "";
+export const SERVICE_KEY_ID = process.env.SERVICE_KEY_ID || '';
+export const SERVICE_KEY_SECRET = process.env.SERVICE_KEY_SECRET || '';
 
 export const PAYTURE_URL =
-  process.env.PAYTURE_URL || "https://sandbox3.payture.com";
+    process.env.PAYTURE_URL || 'https://sandbox3.payture.com';
 export const PAYTURE_TERMINAL_RUB =
-  process.env.PAYTURE_TERMINAL_RUB || "MerchantJustStudyRUB";
+    process.env.PAYTURE_TERMINAL_RUB || 'MerchantJustStudyRUB';
 export const PAYTURE_TERMINAL_USD =
-  process.env.PAYTURE_TERMINAL_USD || "MerchantJustStudyUSD";
+    process.env.PAYTURE_TERMINAL_USD || 'MerchantJustStudyUSD';
 export const PAYTURE_TERMINAL_PASSWORD =
-  process.env.PAYTURE_TERMINAL_PASSWORD || "123";
+    process.env.PAYTURE_TERMINAL_PASSWORD || '123';
 
-export const YOOKASSA_SHOPID = process.env.YOOKASSA_ID || "933369";
+export const YOOKASSA_SHOPID = process.env.YOOKASSA_ID || '933369';
 export const YOOKASSA_SECRET =
-  process.env.YOOKASSA_SECRET ||
-  "test_Wg58Q3yezKSNYZLwcj5wg0otQq4yi0_yCVgUdbM3sB0";
+    process.env.YOOKASSA_SECRET ||
+    'test_Wg58Q3yezKSNYZLwcj5wg0otQq4yi0_yCVgUdbM3sB0';
--- config/storage.ts
+++ config/storage.ts
@@ -1,53 +1,53 @@
-import { StorageConfig } from "@keystone-6/core/dist/declarations/src/types/config";
-import { S3_STORAGE_KEY_ID, S3_STORAGE_KEY_SECRET } from "./index";
+import { StorageConfig } from '@keystone-6/core/dist/declarations/src/types/config';
+import { SERVICE_KEY_ID, SERVICE_KEY_SECRET } from './index';
 
 const storage_image_avatars: StorageConfig = {
-  kind: "s3",
-  type: "image",
-  bucketName: "sitejuststudy",
-  accessKeyId: S3_STORAGE_KEY_ID,
-  secretAccessKey: S3_STORAGE_KEY_SECRET,
-  region: "ru-central1",
-  pathPrefix: "user-avatar-",
-  endpoint: "https://storage.yandexcloud.net",
+    kind: 's3',
+    type: 'image',
+    bucketName: 'sitejuststudy',
+    accessKeyId: SERVICE_KEY_ID,
+    secretAccessKey: SERVICE_KEY_SECRET,
+    region: 'ru-central1',
+    pathPrefix: 'user-avatar-',
+    endpoint: 'https://storage.yandexcloud.net'
 };
 
 const storage_product_image: StorageConfig = {
-  kind: "s3",
-  type: "image",
-  bucketName: "sitejuststudy",
-  accessKeyId: S3_STORAGE_KEY_ID,
-  secretAccessKey: S3_STORAGE_KEY_SECRET,
-  region: "ru-central1",
-  pathPrefix: "product-image-",
-  endpoint: "https://storage.yandexcloud.net",
+    kind: 's3',
+    type: 'image',
+    bucketName: 'sitejuststudy',
+    accessKeyId: SERVICE_KEY_ID,
+    secretAccessKey: SERVICE_KEY_SECRET,
+    region: 'ru-central1',
+    pathPrefix: 'product-image-',
+    endpoint: 'https://storage.yandexcloud.net'
 };
 
 const storage_blog_image: StorageConfig = {
-  kind: "s3",
-  type: "image",
-  bucketName: "sitejuststudy",
-  accessKeyId: S3_STORAGE_KEY_ID,
-  secretAccessKey: S3_STORAGE_KEY_SECRET,
-  region: "ru-central1",
-  pathPrefix: "blog-cover-",
-  endpoint: "https://storage.yandexcloud.net",
+    kind: 's3',
+    type: 'image',
+    bucketName: 'sitejuststudy',
+    accessKeyId: SERVICE_KEY_ID,
+    secretAccessKey: SERVICE_KEY_SECRET,
+    region: 'ru-central1',
+    pathPrefix: 'blog-cover-',
+    endpoint: 'https://storage.yandexcloud.net'
 };
 
 const storage_marketing_image: StorageConfig = {
-  kind: "s3",
-  type: "image",
-  bucketName: "sitejuststudy",
-  accessKeyId: S3_STORAGE_KEY_ID,
-  secretAccessKey: S3_STORAGE_KEY_SECRET,
-  region: "ru-central1",
-  pathPrefix: "marketing-",
-  endpoint: "https://storage.yandexcloud.net",
+    kind: 's3',
+    type: 'image',
+    bucketName: 'sitejuststudy',
+    accessKeyId: SERVICE_KEY_ID,
+    secretAccessKey: SERVICE_KEY_SECRET,
+    region: 'ru-central1',
+    pathPrefix: 'marketing-',
+    endpoint: 'https://storage.yandexcloud.net'
 };
 
 export const storage: Record<string, StorageConfig> = {
-  storage_image_avatars,
-  storage_product_image,
-  storage_blog_image,
-  storage_marketing_image,
+    storage_image_avatars,
+    storage_product_image,
+    storage_blog_image,
+    storage_marketing_image
 };
