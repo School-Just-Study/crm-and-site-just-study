6886c2a198719166ef31f8cd826a81a9bd51497b
Nikolay Kamenev <kamenik1@icloud.com>
2022-11-12 20:25:55 +0300

    feat: added component-block

---
+++ component-blocks/index.tsx
@@ -0,0 +1,13 @@
+import { carousel } from './carousel';
+import { hero } from './hero';
+import { callout } from './callout';
+import { quote } from './quote';
+import { youtubeVideo } from './youtube-video';
+
+export const componentBlocks = {
+    carousel,
+    hero,
+    callout,
+    quote,
+    youtubeVideo
+};
