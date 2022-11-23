03f0563a3b413bbe4e87f98176282d516e7e2b0b
Nikolay Kamenev <kamenik1@icloud.com>
2022-11-23 17:43:02 +0300

    fix: lessons in subscription

--- crm/lib/handleCreateUserWithEmailClient.ts
+++ crm/lib/handleCreateUserWithEmailClient.ts
@@ -2,41 +2,44 @@ import { ListHooks } from '@keystone-6/core/dist/declarations/src/types/config/h
 import { Lists } from '.keystone/types';
 import { notifyNewClient } from '../notifications/notifyNewClient';
 
-export const handleCreateUserWithEmailClient: ListHooks<Lists.Client.TypeInfo>["afterOperation"] =
-  async ({ context, item, operation }) => {
-    if (operation === "create") {
-      notifyNewClient(item, context);
+export const handleCreateUserWithEmailClient: ListHooks<Lists.Client.TypeInfo>['afterOperation'] = async ({
+    context,
+    item,
+    operation
+}) => {
+    if (operation === 'create') {
+        notifyNewClient(item, context);
     }
-    if (operation !== "delete") {
-      if (item && item.email) {
-        let user = await context.query.User.findOne({
-          where: { email: item.email },
-          query: `id`,
-        });
+    if (operation !== 'delete') {
+        if (item && item.email) {
+            const user = await context.query.User.findOne({
+                where: { email: item.email },
+                query: `id`
+            });
 
-        const { name, email, language } = item;
+            const { name, email, language } = item;
 
-        if (!user) {
-          await context.query.User.createOne({
-            data: {
-              name,
-              email,
-              client: { connect: { id: `${item.id}` } },
-              language,
-            },
-            query: `id`,
-          });
-        } else {
-          await context.query.User.updateOne({
-            where: { email },
-            data: {
-              name,
-              email,
-              client: { connect: { id: `${item.id}` } },
-            },
-            query: `id`,
-          });
+            if (!user) {
+                await context.query.User.createOne({
+                    data: {
+                        name,
+                        email,
+                        client: { connect: { id: `${item.id}` } },
+                        language
+                    },
+                    query: `id`
+                });
+            } else {
+                await context.query.User.updateOne({
+                    where: { email },
+                    data: {
+                        name,
+                        email,
+                        client: { connect: { id: `${item.id}` } }
+                    },
+                    query: `id`
+                });
+            }
         }
-      }
     }
-  };
+};
