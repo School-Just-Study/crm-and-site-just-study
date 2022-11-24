46a1900bf762858b4eaae3072b754552c3f72d95
Nikolay Kamenev <kamenik1@icloud.com>
2022-11-24 15:16:50 +0300

    fix: deploy-config [skip ci]

--- crm/config/index.ts
+++ crm/config/index.ts
@@ -1,7 +1,6 @@
 export const SERVER_PORT = parseInt(process.env.PORT!) || 8000;
 
-export const DATABASE_URL =
-    process.env.DATABASE_URL || 'mysql://root:root@localhost:8889/sitejuststudy';
+export const DATABASE_URL = process.env.DATABASE_URL || 'mysql://root:root@localhost:8889/sitejuststudy';
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 export const BACKEND_URL = process.env.BACKEND_URL || 'localhost:8000';
@@ -9,22 +8,15 @@ export const BACKEND_URL = process.env.BACKEND_URL || 'localhost:8000';
 export const NALOG_INN = process.env.NALOG_INN || 0;
 export const NALOG_PASSWORD = process.env.NALOG_PASSWORD || '';
 
-export const SESSION_SECRET =
-    process.env.SESSION_SECRET || 'e21bada5-a1a1-4114-81fd-487be3ad6fca';
+export const SESSION_SECRET = process.env.SESSION_SECRET || 'e21bada5-a1a1-4114-81fd-487be3ad6fca';
 
 export const SERVICE_KEY_ID = process.env.SERVICE_KEY_ID || '';
 export const SERVICE_KEY_SECRET = process.env.SERVICE_KEY_SECRET || '';
 
-export const PAYTURE_URL =
-    process.env.PAYTURE_URL || 'https://sandbox3.payture.com';
-export const PAYTURE_TERMINAL_RUB =
-    process.env.PAYTURE_TERMINAL_RUB || 'MerchantJustStudyRUB';
-export const PAYTURE_TERMINAL_USD =
-    process.env.PAYTURE_TERMINAL_USD || 'MerchantJustStudyUSD';
-export const PAYTURE_TERMINAL_PASSWORD =
-    process.env.PAYTURE_TERMINAL_PASSWORD || '123';
+export const PAYTURE_URL = process.env.PAYTURE_URL || 'https://sandbox3.payture.com';
+export const PAYTURE_TERMINAL_RUB = process.env.PAYTURE_TERMINAL_RUB || 'MerchantJustStudyRUB';
+export const PAYTURE_TERMINAL_USD = process.env.PAYTURE_TERMINAL_USD || 'MerchantJustStudyUSD';
+export const PAYTURE_TERMINAL_PASSWORD = process.env.PAYTURE_TERMINAL_PASSWORD || '123';
 
 export const YOOKASSA_SHOPID = process.env.YOOKASSA_ID || '933369';
-export const YOOKASSA_SECRET =
-    process.env.YOOKASSA_SECRET ||
-    'test_Wg58Q3yezKSNYZLwcj5wg0otQq4yi0_yCVgUdbM3sB0';
+export const YOOKASSA_SECRET = process.env.YOOKASSA_SECRET || 'test_Wg58Q3yezKSNYZLwcj5wg0otQq4yi0_yCVgUdbM3sB0';
--- crm/schemas/avatarUser.schema.ts
+++ crm/schemas/avatarUser.schema.ts
@@ -4,22 +4,22 @@ import { createdAt } from '../fields/createdAt';
 import { lastModification } from '../fields/lastModification';
 
 export const AvatarUser = list({
-  ui: {
-    label: "Аватарки пользователей",
-    isHidden: true,
-  },
-  fields: {
-    user: relationship({ ref: "User.avatar", label: "Клиент" }),
-    image: image({ storage: "storage_image_avatars", label: "Изображение" }),
-    createdAt,
-    lastModification,
-  },
-  access: {
-    operation: {
-      query: () => true,
-      create: ({ session }) => !!session,
-      update: ({ session }) => !!session,
-      delete: ({ session }) => !!session,
+    ui: {
+        label: 'Аватарки пользователей',
+        isHidden: true
     },
-  },
+    fields: {
+        user: relationship({ ref: 'User.avatar', label: 'Клиент' }),
+        image: image({ storage: 'storage_image_avatars', label: 'Изображение' }),
+        createdAt,
+        lastModification
+    },
+    access: {
+        operation: {
+            query: () => true,
+            create: ({ session }) => !!session,
+            update: ({ session }) => !!session,
+            delete: ({ session }) => !!session
+        }
+    }
 });
