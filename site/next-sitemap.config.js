e5af1af7ac143c6251c30a746c299861bf2ebd43
Nikolay Kamenev <kamenik1@icloud.com>
2022-11-27 21:29:17 +0300

    fix: sitemap

--- site/next-sitemap.config.js
+++ site/next-sitemap.config.js
@@ -9,6 +9,16 @@ module.exports = {
     generateRobotsTxt: true,
     generateIndexSitemap: true,
     exclude: excludePages,
+    alternateRefs: [
+        {
+            href: process.env.NEXT_PUBLIC_FRONTEND_URL,
+            hreflang: 'en'
+        },
+        {
+            href: process.env.NEXT_PUBLIC_FRONTEND_URL + '/ru',
+            hreflang: 'ru'
+        }
+    ],
     robotsTxtOptions: {
         policies: [{ disallow: excludePages, userAgent: '*', allow: '/' }],
         additionalSitemaps: sitemaps.map((map) => `${process.env.NEXT_PUBLIC_FRONTEND_URL}/sitemap/${map}`)
