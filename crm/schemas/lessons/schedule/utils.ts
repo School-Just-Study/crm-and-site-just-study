646940b9f3caa07bdfaaf4b95ab41ab6f227b107
Nikolay Kamenev <kamenik1@icloud.com>
2023-05-28 09:40:05 +0300

    fix: try fix timezone for schedule

--- crm/schemas/lessons/schedule/utils.ts
+++ crm/schemas/lessons/schedule/utils.ts
@@ -1,5 +1,5 @@
 import { addWeeks, isFuture, set, setDay } from 'date-fns';
-import { utcToZonedTime } from 'date-fns-tz';
+import { zonedTimeToUtc } from 'date-fns-tz';
 import { localeDate } from '../../../lib/localeDate';
 
 export const checkActive = (endPeriod?: Date | null | undefined) => {
@@ -11,7 +11,7 @@ export const formatWithDateHourMinutes = (dayOfWeek: number, date: Date, time: s
     const [hr, min] = time.split(':').map(Number);
     const setDate = setDay(date, dayOfWeek);
     const dateWithTime = set(setDate, { hours: hr, minutes: min, seconds: 0, milliseconds: 0 });
-    return utcToZonedTime(dateWithTime, timeZone, { locale: localeDate('ru') });
+    return zonedTimeToUtc(dateWithTime, timeZone, { locale: localeDate('ru') });
 };
 
 export const daysForRepeat = [addWeeks(new Date(), 0), addWeeks(new Date(), 1), addWeeks(new Date(), 2)];
