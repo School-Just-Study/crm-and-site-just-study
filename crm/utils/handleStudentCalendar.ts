b279c8b00a6e12177be98f1c3d776e9a20ecb62f
Nikolay Kamenev <kamenik1@icloud.com>
2023-05-28 21:00:56 +0300

    fix: handleStudentCalendar

--- crm/utils/handleStudentCalendar.ts
+++ crm/utils/handleStudentCalendar.ts
@@ -1,6 +1,7 @@
 import { ServerConfig } from '@keystone-6/core/dist/declarations/src/types/config';
 import ical, { ICalAlarmType } from 'ical-generator';
 import { LessonStatus } from '../schemas/lessons/enum';
+import { UserSubscription } from '@src/src/shared/lib/apollo/types';
 
 export const handleStudentCalendar: ServerConfig<any>['extendExpressApp'] = (app, context) => {
     app.get('/api/student/:id/lessons.ical', async (req, res) => {
@@ -14,15 +15,17 @@ export const handleStudentCalendar: ServerConfig<any>['extendExpressApp'] = (app
                 students: { some: { id: { equals: studentId } } },
                 statusLesson: { in: [LessonStatus.Created, LessonStatus.Completed] }
             },
-            query: `id statusLesson startTime endTime teachers { id email name language linkOnlineLesson timeZone } students { id name email } timeZone subscription { name }`
+            query: `id statusLesson startTime endTime teachers { id email name language linkOnlineLesson timeZone } students { id name email } timeZone subscriptions { name student { id } }`
         });
 
         lessons.forEach((lesson) => {
+            const subName = lesson.subscriptions?.find((sub: UserSubscription) => sub?.student?.id === studentId)?.name;
+
             calendar.createEvent({
                 start: new Date(lesson.startTime),
                 end: new Date(lesson.endTime),
                 summary: 'Онлайн-урок в школе Just Study',
-                description: `Обучение по программе: ${lesson?.subscription?.name}, Преподаватель ${lesson.teachers[0]?.name}`,
+                description: `Обучение по программе: ${subName}, Преподаватель ${lesson.teachers[0]?.name}`,
                 url: lesson.teachers[0]?.linkOnlineLesson,
                 alarms: [{ type: ICalAlarmType.audio, triggerBefore: 3600 }]
             });
